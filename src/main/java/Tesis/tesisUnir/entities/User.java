968f65a8ce130991f27113afbd968923205aa415
KennerEspinal <kennerespinalg@gmail.com>
2025-01-13 11:01:28 -0500

    normalizar y Hashear contraseña antes de ingresar a la base de datos.

--- src/main/java/Tesis/tesisUnir/entities/User.java
+++ src/main/java/Tesis/tesisUnir/entities/User.java
@@ -3,6 +3,7 @@ package Tesis.tesisUnir.entities;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
 @Data
@@ -29,4 +30,29 @@ public class User {
 
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     private Program program;
+
+    @PrePersist
+    @PreUpdate
+    public void prePersist() {
+        if (this.nombre != null) {
+            this.nombre = this.nombre.toLowerCase().trim();
+        }
+        if (this.apellido != null) {
+            this.apellido = this.apellido.toLowerCase().trim();
+        }
+        if (this.correo != null) {
+            this.correo = this.correo.toLowerCase().trim();
+        }
+        if (this.usuario != null) {
+            this.usuario = this.usuario.toLowerCase().trim();
+        }
+        if (this.rol != null) {
+            this.rol = this.rol.toLowerCase().trim();
+        }
+
+        if (this.contrasena != null) {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            this.contrasena = passwordEncoder.encode(this.contrasena);
+        }
+    }
 }
