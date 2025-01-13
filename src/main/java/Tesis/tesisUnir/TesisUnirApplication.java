0678af8f298ccfeafb7680135f9a9ee834bfeb68
Orlandoseoanes <112825174+Orlandoseoanes@users.noreply.github.com>
2025-01-12 19:00:34 -0500

    Add files via upload

---
+++ src/main/java/Tesis/tesisUnir/TesisUnirApplication.java
@@ -0,0 +1,13 @@
+package Tesis.tesisUnir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisUnirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesisUnirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tesis/tesisUnir/TesisUnirApplicationTests.java
@@ -0,0 +1,13 @@
+package Tesis.tesisUnir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisUnirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
